6812ccfd9482c92cf8690a98ca0dbd2575576f5a
Deepanshu Pandey <deepanshupandey144@gmail.com>
2024-07-12 16:10:59 +0530

    emotion detectio  app using tflite

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:camera/camera.dart';
+import 'package:flutter/material.dart';
+
+import 'home.dart';
+
+List<CameraDescription>? cameras;
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  cameras = await availableCameras();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Home(),
+    );
+  }
+}
